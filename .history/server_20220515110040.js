78a8d893a3abafabe54e47fca032e03047ddd8a4
lakindulk <lakindulk9@gmail.com>
2022-05-16 13:31:28 +0530

    Deployment and service update

--- .history/server_20220515110040.js
+++ .history/server_20220515110040.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 const URI = process.env.MONGODB_URI;
 
 mongoose
